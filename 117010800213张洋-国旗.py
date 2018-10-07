067a7dd6889a998485a220540245f2ffeae1b857
zhangyaag <43267047+zhangyaag@users.noreply.github.com>
2018-10-07 14:36:18 +0800

    117010800213国旗
    国旗

---
+++ 117010800213张洋-国旗.py
@@ -0,0 +1,54 @@
+import turtle
+
+turtle.bgcolor("red")
+turtle.fillcolor("yellow")
+turtle.color('yellow')
+turtle.speed(10)
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-600,220) 
+turtle.down()
+for i in range (5):    
+    turtle.forward(150)
+    turtle.right(144)
+turtle.end_fill()
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-400,295)
+turtle.setheading(305)
+turtle.down()
+for i in range (5):    
+    turtle.forward(50)
+    turtle.left(144)
+
+turtle.end_fill()
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-350,212)
+turtle.setheading(30)
+turtle.down()
+for i in range (5):  
+    turtle.forward(50)
+    turtle.right(144)
+
+turtle.end_fill()
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-350,145)
+turtle.setheading(5)
+turtle.down()
+for i in range (5):   
+    turtle.forward(50)
+    turtle.right(144)
+
+turtle.end_fill()
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-400,90)
+turtle.setheading(300)
+turtle.down()
+for i in range (5):  
+    turtle.forward(50)
+    turtle.left(144)
+
+turtle.end_fill()
